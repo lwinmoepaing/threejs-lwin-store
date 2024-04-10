5fa130d8247fa73ff3308bf0b2ae24385a617c7f
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2024-04-10 09:08:59 +0700

    add(middleware): for auth

---
+++ src/lib/auth/auth-edge.ts
@@ -0,0 +1,59 @@
+import { NextAuthConfig } from "next-auth";
+
+const nextAuthEdgeConfig = {
+  trustHost: true,
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    maxAge: 30 * 24 * 60 * 60,
+    strategy: "jwt",
+  },
+  providers: [],
+  callbacks: {
+    authorized: ({ auth, request }: any) => {
+      // Run every request with middleware
+      const isLoggedIn = Boolean(auth?.user);
+      const pathName = request.nextUrl.pathname;
+      const isTryingToAdminRoute = pathName.includes("/admin");
+      const isTryingToAuthRote =
+        pathName.includes("/login") || pathName.includes("/sign-up");
+
+      if (!isLoggedIn && isTryingToAdminRoute) {
+        return false;
+      }
+
+      if (isLoggedIn && isTryingToAdminRoute) {
+        return true;
+      }
+
+      if (isLoggedIn && isTryingToAuthRote) {
+        return Response.redirect(new URL("/admin", request.nextUrl));
+      }
+
+      return true;
+    },
+    jwt: ({ token, user, trigger }: any) => {
+      if (user) {
+        // When user sign in
+        token.userId = user.id as string;
+        token.email = user.email as string;
+      }
+
+      if (trigger === "update") {
+        // When requesting update
+      }
+
+      return token;
+    },
+    session: ({ session, token}: any) => {
+      if (session) {
+        session.user.id = token.userId;
+      }
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export default nextAuthEdgeConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,8 @@
+import NextAuth from "next-auth";
+
+import nextAuthEdgeConfig from "./lib/auth/auth-edge";
+
+export default NextAuth(nextAuthEdgeConfig).auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
